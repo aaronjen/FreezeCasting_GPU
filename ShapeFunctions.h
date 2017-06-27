44f264fee74c75c4c92b124d5f0c30de07826cdd
MicDonald <mk60503mk60503@gmail.com>
2017-06-27 13:11:14 -0400

    cu to cpp

--- ShapeFunctions.h
+++ ShapeFunctions.h
@@ -7,16 +7,16 @@ Eigen::RowVectorXd ShapeFunction(double xi, double eta, std::bitset<8> bitElemen
 
 Eigen::MatrixXd NaturalDerivatives(double xi, double eta, std::bitset<8> bitElementType);
 
-__device__ __host__ Eigen::RowVectorXd ShapeFunction(double xi, double eta, unsigned char bitElementType);
+Eigen::RowVectorXd ShapeFunction(double xi, double eta, unsigned char bitElementType);
 
-__device__ __host__ Eigen::MatrixXd NaturalDerivatives(double xi, double eta, unsigned char bitElementType);
+Eigen::MatrixXd NaturalDerivatives(double xi, double eta, unsigned char bitElementType);
 
-__device__ __host__ Eigen::Matrix2d Jacobian(const Eigen::MatrixXd& nodeCoord, const Eigen::MatrixXd& naturalDerivatives);
+Eigen::Matrix2d Jacobian(const Eigen::MatrixXd& nodeCoord, const Eigen::MatrixXd& naturalDerivatives);
 
-__device__ __host__ Eigen::Matrix2d invJacobian(const Eigen::MatrixXd& nodeCoord, const Eigen::MatrixXd& naturalDerivatives);
+Eigen::Matrix2d invJacobian(const Eigen::MatrixXd& nodeCoord, const Eigen::MatrixXd& naturalDerivatives);
 
-__device__ __host__ Eigen::MatrixXd XYDerivatives(const Eigen::MatrixXd& nodeCoord, const Eigen::MatrixXd& naturalDerivatives);
+Eigen::MatrixXd XYDerivatives(const Eigen::MatrixXd& nodeCoord, const Eigen::MatrixXd& naturalDerivatives);
 
-__device__ __host__ double detJacobian(const Eigen::MatrixXd& nodeCoord, const Eigen::MatrixXd& naturalDerivatives);
+double detJacobian(const Eigen::MatrixXd& nodeCoord, const Eigen::MatrixXd& naturalDerivatives);
 
 #endif
